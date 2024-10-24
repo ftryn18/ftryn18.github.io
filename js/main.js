726ae156ce93441b33cd0c89a39839a94ab7480e
ftryn18 <fitri18yani10@gmail.com>
2024-10-24 20:47:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('TADAA!! This flower for you:v semangat terus ya untuk menjalani hari-hari mu.').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
