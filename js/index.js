44e72d2f8e2173ecc56bc33b32fc1e4a17b7bf49
ftryn18 <fitri18yani10@gmail.com>
2024-10-24 20:45:24 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'SELAMAT HARI LAHIR IPEHH SAYANG!! Aku ada hadiah buat kamu klik dibawah ini ya:v'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
